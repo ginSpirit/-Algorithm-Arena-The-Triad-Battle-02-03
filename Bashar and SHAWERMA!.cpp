27a35c26b1de4607188f0b3169507f2467306687
Md. Maksudur Rahman <130817141+ginSpirit@users.noreply.github.com>
2024-10-03 01:06:19 +0600

    C- Create Bashar and SHAWERMA!.cpp

---
+++ Bashar and SHAWERMA!.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define input(v) for(auto &i : v) cin >> i
+#define endl "\n"
+#define all(x)  x.begin(), x.end()
+
+typedef long long ll;
+typedef long long int int64;
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;
+using namespace std;
+
+signed main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+    int x; cin >> x;
+    cout << (2 * x) << endl;
+
+    return (EXIT_SUCCESS);
+}
