90439af970a36efbf80cdee3167dc3b785fdc1e3
Md. Maksudur Rahman <130817141+ginSpirit@users.noreply.github.com>
2024-10-03 01:17:10 +0600

    Update B- Racetrack.cpp

--- B- Racetrack.cpp
+++ B- Racetrack.cpp
@@ -1,1 +1,20 @@
-h
+#include <bits/stdc++.h>
+#define input(v) for(auto &i : v) cin >> i
+#define endl "\n"
+#define all(x)  x.begin(), x.end()
+
+typedef long long ll;
+typedef long long int int64;
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;
+using namespace std;
+
+
+signed main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+    int x, y; cin >> x >> y;
+    cout << (1LL*x*y) / (__gcd(x, y)) << endl;
+
+    return (EXIT_SUCCESS);
+}
